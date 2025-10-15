85049f71ecc3566173bfac53db930a9bbbcfe119
Nihar <nihar.jachak@gmail.com>
2025-10-15 15:48:20 +0530

    Updates in Code inorder to pass testcases

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,6 +5,15 @@ public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
 
+    public Boat() {
+        super();
+    }
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return this.name;

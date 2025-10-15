b6122d697fd6f2249918fdd8ef8a71ed8d92e4f6
Nihar <nihar.jachak@gmail.com>
2025-10-15 15:38:54 +0530

    Implemented the driver code for Vehicle, Car ,F1 classes and created a new class Boat that implements WaterVehicle interface to override the methods.

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -11,15 +11,15 @@ public class Car extends Vehicle {
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
+        super(name);
     }
 
     public void changeGear(int newGear){
-
+        currentGear=newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
